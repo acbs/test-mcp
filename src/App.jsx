eafaa179372b8b4f054439de05036e4a71279f54
André Carlos <andre.carlos1993@hotmail.com>
2025-07-07 14:10:53 -0300

    Versioning configuration and entry point files.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppRoutes from './routes';
+
+const App = () => {
+  return (
+    <div className='container mx-auto'>
+      <AppRoutes />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import App from './App';
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      <App />
+    </Router>
+  </QueryClientProvider>,
+  document.getElementById('root')
+);
